718146315bfd84469a53f23969b678ba03124562
inayattgogia <75385844+inayattgogia@users.noreply.github.com>
2021-01-21 15:26:39 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+function setup() {
+	createCanvas(800, 700);
+rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+	dustbinObj=new dustbin(1200,650);
+	paperObject=new paper(200,450,40);
+	groundObject=new ground(width/2,670,width,20);
+
+	var render = Render.create({
+		element: document.body,
+		engine: engine,
+		options: {
+		  width: 1200,
+		  height: 700,
+		  wireframes: false
+		}
+	  });
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  dustbinObj.display();
+  paperObject.display();
+  groundObject.display();
+}
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+    
+  	}
+  drawSprites();
+ 
+}
+
+
+
